99abc05c980033bec7ee380014b389045764320f
IgorGarciaCosta <igonildo7@gmail.com>
2021-01-13 10:42:43 -0300

    pegando dado da API

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-alert('deu bom ')
+function consultaCep(){
+    $.ajax({
+        url:"https://viacep.com.br/ws/48700000/json",
+        type:"GET",
+        success: function(response){
+            console.log(response);
+        }
+    })
+}
+//https://viacep.com.br/ws/48700000/json
